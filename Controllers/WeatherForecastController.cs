1cd35e1a29c787a04a8c4884833486366ea0aeba
jbrown <jordan@brown.com>
2020-01-22 07:12:13 -0700

    Setup app using a hardcoded set of WeatherForecasts to get structure of app working.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WeatherSearch.Models;
+
+namespace WeatherSearch.Controllers
+{
+    public class WeatherForecastController : Controller
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+            List<WeatherForecast> weatherForecasts = new List<WeatherForecast> {
+                new WeatherForecast { Date = DateTime.Now.AddDays(1), Summary = "sunny", TemperatureC = 5 }
+                , new WeatherForecast { Date = DateTime.Now.AddDays(2), Summary = "partly cloudy", TemperatureC = 10 }
+                , new WeatherForecast { Date = DateTime.Now.AddDays(3), Summary = "sunny", TemperatureC = 12 }
+                , new WeatherForecast { Date = DateTime.Now.AddDays(4), Summary = "cloudy", TemperatureC = 11 }
+                , new WeatherForecast { Date = DateTime.Now.AddDays(5), Summary = "sunny", TemperatureC = 19 }
+            };
+            return View(weatherForecasts);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# net_core_weather_forecast
+Simple .Net Core MVC app leveraring MetaWeather API to display 5 day forecast.
